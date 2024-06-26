b3d8981b8c21d4e592800d5a2c0469640dff51f4
Promish Gurung <promishgrg98@gmail.com>
2024-06-26 19:17:15 +0545

    add default css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export function NavBar() {
     const [activeLink, setActiveLink] = useState('home');
@@ -30,7 +34,7 @@ export function NavBar() {
         <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src='' alt='Logo' />
+                    <img src={logo} alt='Logo' />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" >
                     <span className='navbar-toggle-icon'></span>
@@ -61,9 +65,9 @@ export function NavBar() {
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
-                            <a href='#'><img src='' alt='' /></a>
-                            <a href='#'><img src='' alt='' /></a>
-                            <a href='#'><img src='' alt='' /></a>
+                            <a href='#'><img src={navIcon1} alt='' /></a>
+                            <a href='#'><img src={navIcon2} alt='' /></a>
+                            <a href='#'><img src={navIcon3} alt='' /></a>
                         </div>
                         <button className='vvd' onClick={() => console.log('connect')}>
                             <span>Let's Connect</span>
