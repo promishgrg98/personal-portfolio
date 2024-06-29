55bfba3abbbd72705f83e3cbb2b11a42ff9cbbf7
Promish Gurung <promishgrg98@gmail.com>
2024-06-29 21:18:52 +0545

    create route pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
+import React from 'react';
 import './App.css';
-import { Banner } from './components/Banner';
-import { NavBar } from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
+    <div>
     </div>
   );
 }
