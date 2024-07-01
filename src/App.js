debe4b3c0baa665e33f60abcaa4c7cd582b997f0
Promish Gurung <promishgrg98@gmail.com>
2024-07-01 18:05:42 +0545

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' Component={Home}/>
+        <Route path='/project' Component={Project}/>
+        <Route path='/about' Component={About}/>
+        <Route path='/contact' Component={Contact}/>
+      </Routes>
     </div>
   );
 }
