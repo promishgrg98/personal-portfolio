999ebc0a1a90325af08a1746ba5786dc8a8abd13
Promish Gurung <promishgrg98@gmail.com>
2024-07-02 15:46:06 +0545

    create navbar

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from './pages/Home';
 import Project from './pages/Project';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path='/' Component={Home}/>
         <Route path='/project' Component={Project}/>
