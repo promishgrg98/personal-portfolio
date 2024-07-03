976a029b34c60d9c6a201307b732e607ae99f9da
Promish Gurung <promishgrg98@gmail.com>
2024-07-03 13:42:09 +0545

    style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { FaBars } from "react-icons/fa"
 
 const Navbar = () => {
-  return (
-    <div className='header'>
-        <Link to={'/'}>
-            <h1>Portfolio</h1>
-        </Link>
+    return (
+        <div className='header'>
+            <Link to={'/'}>
+                <h1>Portfolio</h1>
+            </Link>
 
-        <ul>
-            <li>
-                <Link to={'/'}>Home</Link>
-            </li>
-            <li>
-                <Link to={'project'}>Project</Link>
-            </li>
-            <li>
-                <Link to={'about'}>About</Link>
-            </li>
-            <li>
-                <Link to={'contact'}>Contact</Link>
-            </li>
-        </ul>
-    </div>
-  )
+            <ul className='nav-menu'>
+                <li>
+                    <Link to={'/'}>Home</Link>
+                </li>
+                <li>
+                    <Link to={'project'}>Project</Link>
+                </li>
+                <li>
+                    <Link to={'about'}>About</Link>
+                </li>
+                <li>
+                    <Link to={'contact'}>Contact</Link>
+                </li>
+            </ul>
+
+            <div className="hamburger">
+                <FaBars style={{color: '#fff'}} />
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
