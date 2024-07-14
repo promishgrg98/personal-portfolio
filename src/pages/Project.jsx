328dc3869613e5aeb159616f8c7752bcd3186add
Promish Gurung <promishgrg98@gmail.com>
2024-07-14 10:32:13 +0545

    create pricing card

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero2 from '../components/Hero2'
+import PricingCard from '../components/PricingCard'
 
 const Project = () => {
   return (
     <div>
       <Hero2 heading='PROJECTS.' text='Some of my most recent works'/>
+      <PricingCard />
     </div>
   )
 }
