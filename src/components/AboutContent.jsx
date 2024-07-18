df2d0981654e7d31992fd1ea204bdef9dfeaba01
Promish Gurung <promishgrg98@gmail.com>
2024-07-18 20:01:34 +0545

    create about content

---
+++ src/components/AboutContent.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './AboutContent.css'
+import { Link } from 'react-router-dom'
+import React1 from '../assets/react1.jpg'
+import React2 from '../assets/react2.webp'
+
+function AboutContent() {
+  return (
+    <div className='about-content'>
+        <div className="left">
+            <h1>Who Am I?</h1>
+            <p>
+                I'm a react front-end developer. I create responsive secure websites for my clients.
+            </p>
+            <Link to={'/contact'}>
+                <button className='btn'>Contact</button>
+            </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={React1} alt="image" className='img' />
+                </div>
+
+                <div className="img-stack bottom">
+                    <img src={React2} alt="image" className='img' />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutContent
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero2 from '../components/Hero2'
+import AboutContent from '../components/AboutContent'
 
 const About = () => {
   return (
     <div>
       <Hero2 heading='ABOUT ME.' text="Hi! I'm a Web Developer."/>
+      <AboutContent />
     </div>
   )
 }
