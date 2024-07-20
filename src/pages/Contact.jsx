50330a9d8b2214680e03e5b1bd7b21117036d69b
Promish Gurung <promishgrg98@gmail.com>
2024-07-20 17:32:40 +0545

    create form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero2 from '../components/Hero2'
+import Form from '../components/Form'
 
 const Contact = () => {
   return (
     <div>
       <Hero2 heading='CONTACT ME.' text="Let's have a chat."/>
+      <Form />
     </div>
   )
 }
