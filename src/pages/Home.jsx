e3448ddea42f556ad3596329ae4f0701629000a3
Promish Gurung <promishgrg98@gmail.com>
2024-07-20 17:54:33 +0545

    add work card on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Work from '../components/Work'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Work />
     </div>
   )
 }
